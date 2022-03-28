c794a29389d9a00da2d8a1d5947870f10283420f
liuqingfeng <504772813@qq.com>
2022-03-28 19:32:24 +0800

    成功运行本地spark

--- scala/spark_study/spark-core/src/main/java/test_env/wc/spark01_WordCount.scala
+++ scala/spark_study/spark-core/src/main/java/test_env/wc/spark01_WordCount.scala
@@ -3,18 +3,37 @@ import org.apache.spark.{SparkContext,SparkConf}
 //执行的前提必须配置windows spark环境也就是不需要虚拟机了
 object spark01_WordCount {
   def main(args: Array[String]): Unit = {
-    //Spark框架
+//    println(scala.util.Properties.envOrElse("HADOOP_HOME", "empty"))
+//    println(scala.util.Properties.envOrElse("PATH", "empty"))
+        //Spark框架
     //应用程序
     //1.建立与spark的连接
     //JDBC:Connection
-    val master = "spark://192.168.42.128:7077"
-
-//    val remote_file = "hdfs://spark1:9000/user/spark/data/spark.txt"
+    val master = "local"
     val sparkConf=new SparkConf().setMaster(master).setAppName("WordCount")
     val sc=new SparkContext(sparkConf)
 
-    //2.执行业务操作
-    //3.关闭连接
+    //(1)读取文件
+    val lines =sc.textFile("datasets")
+    val words=lines.flatMap(_.split(" "))
+    val wordGroup=words.groupBy(word=>word)
+
+    val wordToCount=wordGroup.map {
+      case (word, list) => {
+        {
+          (word, list.size)
+        }
+      }
+    }
+    val array=wordToCount.collect()
+    array.foreach(println)
+
+//    (2)将一行数据进行拆分,形成一个一个的单词(分词)
+//    (3)将数据分组
+//
+//
+//    2.执行业务操作
+//    3.关闭连接
     sc.stop()
   }
 }
